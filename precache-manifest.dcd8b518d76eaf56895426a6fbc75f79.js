d594fcf08b22b849ff3afd67da10d1c13111574b
tsaketh <tsaketh1107@gmail.com>
2020-08-30 14:40:01 +0530

    Updates

---
+++ precache-manifest.dcd8b518d76eaf56895426a6fbc75f79.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78993463a5da36846f42090aa8c22e49",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "91d906c082cd8613f115",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "84b27d14093548b2c2dd",
+    "url": "/robofriends/static/css/main.57ec7c23.chunk.css"
+  },
+  {
+    "revision": "91d906c082cd8613f115",
+    "url": "/robofriends/static/js/2.67c859f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.67c859f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b27d14093548b2c2dd",
+    "url": "/robofriends/static/js/main.03f8e4c8.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "b68b1c651bab038eff6046413e7a9f1b",
+    "url": "/robofriends/static/media/Zilap Black Storm DEMO.b68b1c65.ttf"
+  },
+  {
+    "revision": "3a9eb038ee0f66e422249c91ef466872",
+    "url": "/robofriends/static/media/loading.3a9eb038.png"
+  }
+]);
